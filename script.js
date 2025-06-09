c6bc6ef5de5c1dff0e0f1e9b52b9ea68235f44fd
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-09 08:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,34 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  if (num === 0) return ""; // Romans didn't have a symbol for 0
 
-  //your code here
-	 let ones = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
-        let tens = ["","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"];
-        let hrns = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"];
-        let ths=["","M","MM","MMM"];
-        
-        return ths[Math.floor(num/1000)] + hrns[Math.floor((num%1000)/100)] + tens[Math.floor((num%100)/10)] + ones[Math.floor(num%10)];
+  const romanNumerals = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1],
+  ];
 
+  let result = "";
+
+  for (const [symbol, value] of romanNumerals) {
+    while (num >= value) {
+      result += symbol;
+      num -= value;
+    }
+  }
+
+  return result;
 }
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
- console.log(convertToRoman(36));
 
 
 
