d6e55c5053ed199c76ff810f2a9f3af88b1aaea7
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-09 08:12:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ function convertToRoman(num) {
     };
 
   //your code here
+	 let ones = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        let tens = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        let hrns = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        let ths={"","M","MM","MMM"};
+        
+        return ths[num/1000] + hrns[(num%1000)/100] + tens[(num%100)/10] + ones[num%10
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+ console.log(convertToRoman(36));
 
 
 
